3d59d2ccaeaace1610aa258bde25c68e6c94ec7e
HowardMui <howardmui.2020@gmail.com>
2023-04-24 05:27:26 +0800

    test: test 18

--- src/components/Home/HomeCom.js
+++ src/components/Home/HomeCom.js
@@ -5,7 +5,7 @@ import { Col, Container, Row } from "react-bootstrap";
 import { Btn } from "../partials/Btn";
 
 const HomeCom = () => {
-  console.log("test: landing page 17");
+  console.log("test: landing page 18");
 
   return (
     <div className="" id="home-div">
